65e0320f77305746948c5e7b3aef3301b67accb0
chaitanya dange <semicd99@gmail.com>
2023-01-01 22:02:37 +0530

    added various classes

--- DAY 02/HR/SalesManager.cs
+++ DAY 02/HR/SalesManager.cs
@@ -6,7 +6,20 @@ public class SalesManager:SalesEmployee{
         Bonus=0;
     }
 
-    public SalesManager(int Id,string FirstName,string LastName,string Email,string ContactNumber,string Location,DateTime Birthday,float DA,float HRA,float BasicSalary,float Target,float SalesDone,float ):base(){
+    public SalesManager(int Id,string FirstName,string LastName,string Email,string ContactNumber,string Location,DateTime Birthday,float DA,float HRA,float BasicSalary,float Target,float SalesDone,float bonus ):base( Id, FirstName,LastName, Email, ContactNumber, Location, Birthday,DA, HRA, BasicSalary, Target, SalesDone){
+        this.Bonus=Bonus;
 
+    }
+
+    public override void DoWork(){
+        base.DoWork();
+    }
+
+    public override float ComputePay(){
+        return base.ComputePay()+Bonus;
+    }
+
+    public string ToString(){
+        return base.ToString()+"Bonus= "+Bonus;
     }
 }
---
+++ DAY 02/Membership/AuthManager.cs
@@ -0,0 +1,18 @@
+﻿namespace Membership;
+public class AuthManager
+{
+
+    public static bool Validate(string email,string password){
+        bool status=false;
+        if(email=="ravi.tambade@transflower.in"&& password=="seed"){
+            status=true;
+        }
+
+        return status;
+    }
+
+    public static bool Register(string firstName,string lastname,string email,string location ,string contactNumber){
+        
+    }
+
+}
